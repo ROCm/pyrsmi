59835584a62dba9c858a490931c6abd4a39ab8e9
Younseo Roh <39789740+younseojava@users.noreply.github.com>
2023-07-13 10:55:09 -0700

    add updated license file

--- examples/system_dashboard/server.py
+++ examples/system_dashboard/server.py
@@ -1,3 +1,24 @@
+# MIT License
+# 
+# Copyright (c) 2023 Advanced Micro Devices, Inc.
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 import sys
 
@@ -21,11 +42,6 @@ if apps.gpu.ngpus > 0:
     routes['/GPU-Memory'] = apps.gpu.gpu_mem
     routes['/GPU-Resources'] = apps.gpu.gpu_resource_timeline
     routes['/System-Resources'] = apps.gpu.system_resource_timeline
-    # if apps.gpu.pci_gen is not None:
-    #     routes["/PCIe-Throughput"] = apps.gpu.pci
-    # if apps.gpu.nvlink_ver is not None:
-    #     routes["/NVLink-Throughput"] = apps.gpu.nvlink
-    #     routes["/NVLink-Timeline"] = apps.gpu.nvlink_timeline
 
 class RouteIndex(web.RequestHandler):
     """ A JSON index of all routes present on the Bokeh Server """
